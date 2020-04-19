d7214f7e3ee3703e49e67cb8cb2eb2968ef14fbe
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2020-04-20 01:58:57 +0200

    .3

--- mixnerdapi/server.py
+++ mixnerdapi/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import youtube_dl
 import subprocess
 import shutil
@@ -7,9 +7,12 @@ import json
 import glob, os
 import dotenv
 import sys
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 dotenv.load_dotenv()
+cors = CORS(app)
 
 config = {
     #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
@@ -20,18 +23,23 @@ config = {
 }
 re = ACRCloudRecognizer(config)
 
-@app.route('/rec', methods = ['POST'])
-def recognize():
 
+@app.route('/rec', methods = ['POST'])
+@cross_origin()
+def recognize():
     data = request.get_json(force=True)
     url = data['url']
-    print(url)
+    print(data['timestamps'])
+    segment_time = 120
+    if(data['timestamps']):
+        segment_time = 15
     temp_cnt = 0
     for fold in os.listdir('.'):
         if(fold.startswith('temp')):
             temp_cnt += 1
     temp_cnt = str(temp_cnt)
     ydl_opts = {
+        'noplaylist': True,
         'format': 'worstaudio/worst',
         'prefer_ffmpeg': True,
         'keepvideo': False,
@@ -40,30 +48,52 @@ def recognize():
     
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-        cmd = 'ffmpeg -i temp' + temp_cnt+ '/file.webm -f segment -segment_time 120 -c copy temp' + temp_cnt + '/out%04d.webm'
+        cmd = 'ffmpeg -i temp' + temp_cnt+ '/file.webm -f segment -segment_time ' + str(segment_time) + ' -c copy temp' + temp_cnt + '/out%04d.webm'
         process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         process.wait()
         os.remove('./temp' + temp_cnt + '/file.webm')
         return_info = []
+        timestamps = []
+        print('gettin')
+        timestamp_cnt = 0
         for file in os.listdir('./temp' + temp_cnt):
+            timestamp_cnt += 1
             if(file.startswith('o')):
                 result = json.loads(re.recognize_by_file('./temp' + temp_cnt + '/' + file, 0))
+                print('ha')
                 if(result):
                     if(result.get('metadata') is None):
                         continue
-                    artist_name = result.get('metadata').get('music')[0].get('artists')[0].get('name')
+                    artists = map(lambda artist: artist.get('name'), result.get('metadata').get('music')[0].get('artists'))
+                    artists = ', '.join(artists)
+                    print(result)
                     song_name = result.get('metadata').get('music')[0].get('title')
-                    return_info.append(artist_name + ' - ' + song_name)
-                    os.remove('./temp' + temp_cnt + '/' + file)
-        return_info = list(dict.fromkeys(return_info))
-        print(return_info)
+                    album_name = result.get('metadata').get('music')[0].get('album').get('name')
+                    print(album_name)
+                    return_info.append({'artists': artists, 'song_name': song_name, 'album_name': album_name})
+                    timestamps.append((timestamp_cnt-1) * 15)
+                os.remove('./temp' + temp_cnt + '/' + file)
+        seen = set()
+        new_l = []
+        index = 0
+        for d in return_info:
+            print(d)
+            t = tuple(d.items())
+            if t not in seen: 
+                a = d.copy()
+                a['timestamp'] = timestamps[index]
+                seen.add(t)
+                new_l.append(a)
+            index += 1
+        return_info = new_l
+
         
         try:
             shutil.rmtree('temp' + temp_cnt)
         except OSError as e:
             print ("Error: %s - %s." % (e.filename, e.strerror))
-    return json.dumps(return_info)
+    return jsonify(return_info)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
---
+++ mixnerdui/src/App.js
@@ -0,0 +1,134 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Toast from "react-bootstrap/Toast";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+import Table from "react-bootstrap/Table";
+
+function App() {
+  var image = require('./assets/man-face-with-headphones-sunglasses-and-beard.png');
+
+  const [url, setUrl] = useState('');
+  const [error, setError] = useState(false);
+  const [dataLoading, setDataLoading] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [receivedData, setReceivedData] = useState({});
+  const [withTimestamps, setWithTimestamps] = useState(false);
+  const [shouldShowTimestams, setShouldShowTimestamps] = useState(false);
+  function handleClick(){
+      if(url.length === 0){
+          setError(true);
+          return;
+      }
+      setReceivedData({});
+      setDataLoaded(false);
+      setDataLoading(true);
+      setError(false);
+      const params = {
+          url: url,
+          timestamps: withTimestamps
+      };
+
+      axios.post('http://127.0.0.1:5000/rec', params).then(res => {
+
+          console.log(res.data.map(el => console.log(el)));
+          setReceivedData(res.data);
+          setDataLoading(false);
+          setDataLoaded(true);
+          setShouldShowTimestamps(withTimestamps);
+          console.log(receivedData);
+      }).catch((error) => {
+          console.log(error);
+          setDataLoading(false);
+          setError(true);
+      })
+
+
+
+  }
+
+    function secondsToMinutesSeconds(time){
+        var minutes = Math.floor(time / 60);
+        var seconds = time - minutes * 60;
+        var hours = Math.floor(time / 3600);
+        time = time - hours * 3600;
+        var finalTime = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+
+        return finalTime
+
+    }
+
+    function str_pad_left(string,pad,length) {
+        return (new Array(length+1).join(pad)+string).slice(-length);
+    }
+
+  return (
+
+    <div className="App">
+      <Navbar className={"navbar"} expand="lg">
+          <div className={"navContainer"}>
+              <Navbar.Brand className={"brand"} href="/">Mixnerd</Navbar.Brand>
+          </div>
+      </Navbar>
+        <div className={"pageContainer"}>
+            <Image src={image} className={"image"}/>
+            <h1 className={"title"}>Mixnerd</h1>
+            <div className={"formContainer"}>
+                <Form className={"mainForm"}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control placeholder="Paste Youtube mixtape link here"
+                                      value={url}
+                                      onChange={e => setUrl(e.target.value)}
+                        />
+                    </Form.Group>
+                </Form>
+                <Button disabled={dataLoading} className={"button"} onClick={handleClick} variant="light">Listen!</Button>{' '}
+                <label className={"timestamplabel"}>Timestamps:</label>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check disabled={dataLoading} type="checkbox" onChange={() => setWithTimestamps(!withTimestamps)}/>
+                </Form.Group>
+            </div>
+            <p className={"tsWarning"}>Plase note that with timestamps, the processing of the file will take a significantly larger amount of time.</p>
+            {error && <Toast  className={"toastInvalid"}>
+                <Toast.Body>There was an error. Please try again.</Toast.Body>
+            </Toast>}
+            {dataLoading && <Spinner className={"spinner"} animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+            </Spinner>}
+            {dataLoaded && <Table className={"songTable"} striped bordered hover variant="dark">
+                <thead>
+                <tr>
+                    <th>Artist(s)</th>
+                    <th>Song</th>
+                    <th>Album</th>
+                    {shouldShowTimestams && <th>Timestamp</th>}
+                </tr>
+                </thead>
+                <tbody>
+                {receivedData.map((element, i) => {
+                    return(
+                        <tr key={i}>
+                            <td>{element.artists}</td>
+                            <td>{element.song_name}</td>
+                            <td>{element.album_name}</td>
+                            {shouldShowTimestams && <td>Around {secondsToMinutesSeconds(element.timestamp)}</td>}
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </Table>}
+        </div>
+    </div>
+  );
+}
+
+export default App;
+
